7e85d63eee14ccef6ced1a661571ecfa260151f9
RISHI BARANWAL <119731943+RISHI-BARANWAL@users.noreply.github.com>
2024-03-24 02:23:19 +0530

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+package calculator;
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Calculator calculator = new Calculator();
+    }
+    
+}
